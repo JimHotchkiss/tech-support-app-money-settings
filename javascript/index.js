3e2af9c967b06d6412e3284eea459eefad71b291
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2020-10-07 10:51:09 -0700

    Adding the msa card.

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   landingPageEventListener();
+  msaEventListener();
 });
 
 const landingPageEventListener = () => {
   const msaButton = document.getElementById("msa-button");
   msaButton.addEventListener("click", () => {
+    location.replace("./msa.html");
+  });
+};
+
+const msaEventListener = () => {
+  const msaDiv = document.getElementById("msa-card");
+  msaDiv.addEventListener("click", () => {
     location.replace("./msa.html");
   });
 };
