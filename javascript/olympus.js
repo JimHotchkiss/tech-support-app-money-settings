7dc676c5e26ebf02dd91682713b313aab6b9bded
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2020-10-23 16:22:23 -0700

    Building olympus page.

--- javascript/olympus.js
+++ javascript/olympus.js
@@ -1,6 +1,53 @@
 document.addEventListener("DOMContentLoaded", () => {
   olympusLandingListener();
+  olympusBtnListener();
 });
+
+const olympusSetttingsState = {
+  specialty: { name: "" },
+};
+
+const olympusBtnListener = () => {
+  const olympusSpecialtyButton = document.getElementsByClassName(
+    "olympus-specialty-button"
+  );
+  for (let item of olympusSpecialtyButton) {
+    item.addEventListener("click", () => {
+      console.log(item.dataset.specialty);
+      let selectedSpecialty = item.dataset.specialty;
+      updateSpecialtyState(selectedSpecialty);
+      updateSpecialtyTitle();
+      hideTitleBtn();
+      showBackArrow();
+      centerLogoDiv();
+    });
+  }
+};
+
+const centerLogoDiv = () => {
+  const olympusLogoDiv = document.getElementById("olympus-logo-div");
+  olympusLogoDiv.classList.add("olympus-logo-div-center");
+};
+
+const showBackArrow = () => {
+  const backArrow = document.getElementById("olympus-back-btn-div");
+  backArrow.classList.add("olympus-back-btn-div-show");
+};
+
+const hideTitleBtn = () => {
+  const olumpusBtnDiv = document.getElementById("olympus-btn-div");
+  olumpusBtnDiv.classList.add("olympus-btn-div-hide");
+};
+
+const updateSpecialtyState = (selectedSpecialty) => {
+  olympusSetttingsState.specialty.name = selectedSpecialty;
+};
+const updateSpecialtyTitle = () => {
+  const olympusSpecialtyTitleSpan = document.getElementById(
+    "olympus-specialty-title-span"
+  );
+  olympusSpecialtyTitleSpan.innerText = olympusSetttingsState.specialty.name;
+};
 
 const olympusLandingListener = () => {
   const olympusHomeIcon = document.getElementById("olympus-home-icon");
