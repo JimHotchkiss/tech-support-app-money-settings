4536ef610780329ced7d8c7bfdd7366734b8db6a
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2020-10-30 06:55:40 -0700

    Building out the Storz settings feature.

--- javascript/storz.js
+++ javascript/storz.js
@@ -1,11 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
   storzLandingListener();
   storzCcuBtnListener();
+  storzMonitorBtnListener();
 });
+
+// Need to add reps name in settings
 
 const storzSetttingsState = {
   ccu: { name: "" },
   monitor: { name: "" },
+  rep: { name: "" },
+};
+
+const storzMonitorBtnListener = () => {
+  const storzMonitorBtn = document.getElementsByClassName(
+    "storz-monitor-button"
+  );
+
+  for (let item of storzMonitorBtn) {
+    item.addEventListener("click", () => {
+      let selectedMonitor = item.dataset.monitor;
+      updateStorzMonitorState(selectedMonitor);
+      updateStorzTitle();
+      hideStorzMonitorBtns();
+    });
+  }
+};
+
+const hideStorzMonitorBtns = () => {
+  const storzMonitorsDiv = document.getElementById("storz-monitors-div");
+  storzMonitorsDiv.classList.remove("storz-monitors-div-show");
+};
+
+const updateStorzMonitorState = (selectedMonitor) => {
+  storzSetttingsState.monitor.name = selectedMonitor;
 };
 
 const storzCcuBtnListener = () => {
@@ -15,19 +43,41 @@ const storzCcuBtnListener = () => {
       let selectedCcu = item.dataset.ccu;
       updateStorzCcuState(selectedCcu);
       updateStorzTitle();
-      hideStorzBtnDiv();
+      showStorzBackArrow();
+      centerStorzLogo();
+      hideStorzCcusBtnDiv();
+      showStorzMonitorsBtn();
     });
   }
 };
 
-const hideStorzBtnDiv = () => {
+const showStorzMonitorsBtn = () => {
+  const storzMonitorsDiv = document.getElementById("storz-monitors-div");
+  storzMonitorsDiv.classList.add("storz-monitors-div-show");
+};
+
+const centerStorzLogo = () => {
+  const storzLogoDiv = document.getElementById("storz-logo-div");
+
+  storzLogoDiv.classList.add("storz-logo-div-center");
+};
+
+const showStorzBackArrow = () => {
+  const backArrow = document.getElementById("storz-back-btn-div");
+  backArrow.classList.add("storz-back-btn-div-show");
+};
+
+const hideStorzCcusBtnDiv = () => {
   const storzBtnDiv = document.getElementById("storz-btn-div");
   storzBtnDiv.classList.add("storz-btn-div-hide");
 };
 
 const updateStorzTitle = () => {
   const storzTitleText = document.getElementById("storz-title-text");
-  storzTitleText.innerText = storzSetttingsState.ccu.name;
+  storzTitleText.innerText =
+    storzSetttingsState.ccu.name + " " + storzSetttingsState.monitor.name;
+  const strozRepDiv = document.getElementById("storz-reps-div");
+  strozRepDiv.innerText = storzSetttingsState.rep.name;
 };
 
 const updateStorzCcuState = (selectedCcu) => {
